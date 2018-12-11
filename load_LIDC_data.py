3ad908b7cb317eb6168beb9864b3a4d2466505d2
Stefan <stefanknegt@gmail.com>
2018-12-11 10:14:17 +0100

    Added dataloader for LIDC dataset

---
+++ load_LIDC_data.py
@@ -0,0 +1,76 @@
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+import numpy as np
+import os
+import random
+import pickle
+
+class LIDC_IDRI(Dataset):
+    images = []
+    labels = []
+    series_uid = []
+
+    def __init__(self, dataset_location, transform=None):
+        self.transform = transform
+        max_bytes = 2**31 - 1
+        data = {}
+        for file in os.listdir(dataset_location):
+            filename = os.fsdecode(file)
+            if '.pickle' in filename:
+                print("Loading file", filename)
+                file_path = dataset_location + filename
+                bytes_in = bytearray(0)
+                input_size = os.path.getsize(file_path)
+                with open(file_path, 'rb') as f_in:
+                    for _ in range(0, input_size, max_bytes):
+                        bytes_in += f_in.read(max_bytes)
+                new_data = pickle.loads(bytes_in)
+                data.update(new_data)
+        
+        for key, value in data.items():
+            only_zeros = 0
+            x = value['image'].astype(float)
+            for m in value['masks']:
+                if np.count_nonzero(m) == 0:
+                    only_zeros += 1
+
+            #Here we ignore images where 2 or more annotators annotate no nodule
+            if only_zeros < 2:
+                self.images.append(x)
+                self.labels.append(value['masks'])
+                self.series_uid.append(value['series_uid'])
+
+        assert (len(self.images) == len(self.labels) == len(self.series_uid))
+
+        for img in self.images:
+            assert np.max(img) <= 1 and np.min(img) >= 0
+        for label in self.labels:
+            assert np.max(label) <= 1 and np.min(label) >= 0
+
+        del new_data
+        del data
+
+    def __getitem__(self, index):
+        image = np.expand_dims(self.images[index], axis=0)
+
+        #Randomly select one of the four labels for this image
+        label = self.labels[index][random.randint(0,3)].astype(float)
+        if self.transform is not None:
+            image = self.transform(image)
+
+        series_uid = self.series_uid[index]
+
+        # Convert image and label to torch tensors
+        image = torch.from_numpy(image)
+        label = torch.from_numpy(label)
+
+        #Convert uint8 to float tensors
+        image = image.type(torch.FloatTensor)
+        label = label.type(torch.FloatTensor)
+
+        return image, label, series_uid
+
+    # Override to give PyTorch size of dataset
+    def __len__(self):
+        return len(self.images)

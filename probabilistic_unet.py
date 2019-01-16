94e0559b780fe65c65aafccad4b12db91238fb19
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-01-16 09:45:29 +0100

    Update probabilistic_unet.py
    Change beta value to reflect a good starting point and don't get into unstability (derived from #3 )

--- probabilistic_unet.py
+++ probabilistic_unet.py
@@ -187,7 +187,7 @@ class ProbabilisticUnet(nn.Module):
     no_cons_per_block: no convs per block in the (convolutional) encoder of prior and posterior
     """
 
-    def __init__(self, input_channels=1, num_classes=1, num_filters=[32,64,128,192], latent_dim=6, no_convs_fcomb=4, beta=1.0):
+    def __init__(self, input_channels=1, num_classes=1, num_filters=[32,64,128,192], latent_dim=6, no_convs_fcomb=4, beta=10.0):
         super(ProbabilisticUnet, self).__init__()
         self.input_channels = input_channels
         self.num_classes = num_classes
